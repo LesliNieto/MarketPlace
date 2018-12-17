6723313c91b88a74e4708272c5b7fcdca4437a5b
Lina Gallardo <lina.gallardo@koombea.com>
2018-12-17 11:32:31 -0500

    Creating User model

--- db/schema.rb
+++ db/schema.rb
@@ -24,4 +24,14 @@ ActiveRecord::Schema.define(version: 2018_12_17_162232) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "cellphone"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
