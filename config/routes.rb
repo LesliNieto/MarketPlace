1362734879b2c6f7ac1d619db874fb1e5442415f
Lesli Nieto <lesli.nieto@koombea.com>
2018-12-19 17:35:12 -0500

    Creating show action in the product controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'products#index'
 
-  resources :products, only: [:index, :new, :create]
+  resources :products, except: [:edit, :update, :destroy, :delete ]
   resources :users, only: [:index, :new, :create]
 
 end
