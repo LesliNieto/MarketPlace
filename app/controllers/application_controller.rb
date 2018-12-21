e819f993610dd12151f71fca5db7381ac07259dd
Lina Gallardo <lina.gallardo@koombea.com>
2018-12-21 11:47:11 -0500

    Update user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :cellphone, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :cellphone, :address])
   end
 
 end
