1518a24dabd61a36f043829fb0731af9f3fcbcc6
Lina Gallardo <lina.gallardo@koombea.com>
2019-01-21 18:19:21 -0500

    Changes requested

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  before(:each) do
-    @user1 = create(:user)
-    @user2 = User.create_form_provider_data(RecursiveOpenStruct.new(OmniAuth::config.mock_auth[:facebook]))
-    @user3 = User.create_form_provider_data(RecursiveOpenStruct.new(OmniAuth::config.mock_auth[:google]))
-  end
-
   describe "Associations" do
     it { should have_many(:products).dependent(:destroy) }
   end
@@ -16,61 +10,70 @@ RSpec.describe User, type: :model do
     it { should accept_nested_attributes_for(:products) }
   end
 
-  describe "#Create" do
-
-    describe "When creating a user" do
-      it "must respond positively" do
-        expect(@user1).to be_valid
-      end
-
-      it "validates that the user is created" do
-        expect(@user1.last_name).to eql("Test")
-      end
-    end
-
-    describe "when creating a user without the fields required" do
-
-      it { should validate_presence_of(:first_name) }
-      it { should validate_presence_of(:last_name) }
-      it { should validate_presence_of(:email) }
-      it { should validate_presence_of(:password)}
+  describe "Validations" do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password)}
+    
+    context 'duplicated emails' do
+      let!(:user) { create(:user, email: "test@gmail.com") }
       
       it "is invalid when enter an email that is already in use" do
         user = build(:user, email: "test@gmail.com")
         user.valid?
         expect(user.errors[:email]).to include("has already been taken")
       end
+    end
 
-      it "is invalid when enter a  different password confirmation" do
-        user = build(:user, password: "123456789", password_confirmation: "789456123")
-        user.valid?
-        expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    it "is invalid when enter a  different password confirmation" do
+      user = build(:user, password: "123456789", password_confirmation: "789456123")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it "is invalid when enter a password too short" do
+      user = build(:user, password: "123456")
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 8 characters)")
+    end
+  end
+
+  describe "#Create" do
+
+    describe "When creating an user via the 'user' factory" do
+      let(:user) { create(:user) }
+      
+      it "must respond positively" do
+        expect(user).to be_valid
       end
 
-      it "is invalid when enter a password too short" do
-        user = build(:user, password: "123456")
-        user.valid?
-        expect(user.errors[:password]).to include("is too short (minimum is 8 characters)")
+      it "validates that the user is created" do
+        expect(user.last_name).to eql("Test")
       end
     end
 
     describe "#Login facebook" do
+      let(:user) { User.create_form_provider_data(RecursiveOpenStruct.new(OmniAuth::config.mock_auth[:facebook])) }
+
       it "data provider" do
-        expect(@user2).to be_persisted
+        expect(user).to be_persisted
       end
 
       it "validates if user is create" do
-        expect(@user2.first_name).to eql("Joe")
+        expect(user.first_name).to eql("Joe")
       end
     end
 
     describe "#Login google" do
+      let(:user) { User.create_form_provider_data(RecursiveOpenStruct.new(OmniAuth::config.mock_auth[:google])) }
+
       it "data provider" do
-        expect(@user3).to be_persisted
+        expect(user).to be_persisted
       end
 
       it "validates if user is create" do
-        expect(@user3.first_name).to eql("Laura")
+        expect(user.first_name).to eql("Laura")
       end
     end
 
