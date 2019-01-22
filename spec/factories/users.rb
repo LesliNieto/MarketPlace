7fe4eedc77bee9ed395f3adce26ff7e6f3137900
Lesli Nieto <lesli.nieto@koombea.com>
2019-01-22 16:37:52 -0500

    Fix factories for product and users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name { "Test" }
     last_name { "Test" }
-    email { "test@gmail.com" }
+    sequence(:email) { |n| "test#{n}@gmail.com" }
     cellphone { "1234567895" }
     address { "Barranquilla" } 
     password { "123456789" }
